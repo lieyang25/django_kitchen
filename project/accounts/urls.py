da3546df5a7a33c9c672e8ddf935126c36bcc0bc
凉宫 <@.com>
2024-10-27 21:39:44 +0800

    urls添加命名空间

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import register_view, login_view, logout_view
 from . import views
+
+app_name = 'accounts'  # 设置命名空间
+
 urlpatterns = [
     path('',views.index,name='index'),
     path('register/', register_view, name='register'),
