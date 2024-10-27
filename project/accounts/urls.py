9d9798d4fcd07ced37e462ef5ed66c4f6d6ccb02
凉宫 <@.com>
2024-10-27 21:31:11 +0800

    初步完成

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    #新加入的
+    path('page/',views.page,name='page'),
+    path('pizzas/',views.pizzas,name='pizzas'),
+    path('pizzas/<int:pizza_id>',views.pizza,name='pizza')
 ]
