41445039df42a78ba7efc951a7e97887e4b81a57
凉宫 <@.com>
2024-10-27 20:42:32 +0800

    模型复原为披萨

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Talk(models.Model):
-    """我的话题"""
+class Pizza(models.Model):
+    """我的披萨"""
     name = models.CharField(max_length=20)  #名称不超过20个字符串
     time_added = models.DateTimeField(auto_now_add=True)    #自动添加时间
 
     def __str__(self):
-        """话题名"""
+        """返回披萨名"""
         return self.name
 
 class Entry(models.Model):
-    """讨论条目"""
+    """披萨配方"""
     #后面那个会在删除Pizza的时候同时删除相关Entry
-    topic = models.ForeignKey(Talk,on_delete=models.CASCADE)
+    topic = models.ForeignKey(Pizza,on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
